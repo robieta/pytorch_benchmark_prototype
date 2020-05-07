dee79d29a3d1138285d2f7508322e98da1b75979
Taylor Robie <taylorrobie@fb.com>
2020-05-07 10:46:21 -0700

    clean up intermediante timeit example

--- examples/intermediate_timeit.py
+++ examples/intermediate_timeit.py
@@ -9,14 +9,13 @@ import utils as benchmark_utils
 
 def main():
     class MyExampleGenerator(benchmark_utils.ExampleGenerator):
-        default_number = 5
         def take(self, n):
             for i in range(n):
                 size = (i + 1) ** 6
                 yield benchmark_utils.Example(
                     globals={"x": torch.ones((size, 8)), "y": torch.ones((1, 8))},
-                    description=f"({size}, 8) + (1, 8)",
-                    metadata={"size": size, "num_elements": size * 8})
+                    description=None,
+                    metadata={"num_elements": size * 8})
 
     timer = benchmark_utils.Timer(
         stmt="x + y",
